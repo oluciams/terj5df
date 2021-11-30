7082b25fcd65014452a293b959c3ce975a3dc7c2
olga <oluciams84@gmail.com>
2021-11-30 15:06:09 -0500

    Agrega map, para mostrar los invitados y onSubmit para enviar los datos del formulario, componente de clase

--- src/App.js
+++ src/App.js
@@ -1,44 +1,53 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 
-const App = ()=> {
-
-  const [firstName, setFirstName] = useState('');
-  const [lastName, setLastName] = useState('');
-  const [guests, setGuests] = useState([]);
-
-  const handleFirstName = (e)=>{    
-    setFirstName(e.target.value)
-  }
-
-  const handleLastName = (e)=>{   
-    setLastName(e.target.value)
-  }
-
-  const handleOnSubmit = (e)=>{
-    e.preventDefault()
-
-
-    let guest = {}
-    guest.firstName = firstName
-    guest.lastname = lastName
-
-    setGuests([...guests, guest]) 
-
-  }
+class App extends Component {
   
+  constructor(props) {
+    super(props); 
+    this.state = {
+      users: [],
+      name: '',
+      lastname: ''
+    }
+  }
+
+  addUser(event){  
+    event.preventDefault();
+    
+    const objID = this.state.users.length === 0 ? 1 : this.state.users.length + 1  
+    this.setState({
+      users: this.state.users.concat({id: objID, name: this.state.name, lastname: this.state.lastname})
+    }) 
+    event.target.reset()
+    
+  }
+
+  updateUsername (event) {
+    this.setState({
+      name: event.target.value
+    })
+  }
+
+  updateUserlastname (event){
+    this.setState({
+      lastname: event.target.value
+    })
+  }
+
+  render() {
     return (
-      <div className="container">
+      <div className="container"> 
         <div className="row">
           <div className="col-sm-6 col-sm-offset-3">
-            <form onSubmit={handleOnSubmit}>
+            <form onSubmit={this.addUser.bind(this)}>             
               <div className="form-group">
-                <label htmlFor="firstName">Nombre</label>
-                <input type="text" value={firstName} onChange={handleFirstName} className="form-control" name="first-name" />
+                <label htmlFor="first-name">Nombre</label>
+                <input type="text" className="form-control" name="first-name" onChange={this.updateUsername.bind(this)}/>
               </div>
 
               <div className="form-group">
-                <label htmlFor="lastName">Apellido</label>
-                <input type="text" value={lastName} onChange={handleLastName} className="form-control" name="last-name" />
+                <label htmlFor="last-name">Apellido</label>
+                <input type="text" className="form-control" name="last-name" onChange={this.updateUserlastname.bind(this)}/>
               </div>
 
               <div className="action">
@@ -51,70 +60,26 @@ const App = ()=> {
                 <tr>
                   <th>Nombre</th>
                   <th>Apellido</th>
-                </tr>                
-              </thead>              
+                </tr>
+              </thead>
               <tbody>
-                {
-                guests.map((guest, index)=>
-                <tr key={index}>
-                  <td>{guest.firstName}</td>
-                  <td>{guest.lastname}</td>
-                </tr>            
-                )}                
+                {this.state.users.map((user) =>
+                  <tr key={user.id}>
+                    <td>
+                      {user.name}                   
+                    </td>  
+                    <td>
+                      {user.lastname}
+                    </td>  
+                  </tr>
+                )}
               </tbody>
             </table>
           </div>
         </div>
       </div>
     )
-  
+  }
 }
 
 export default App
-
-// import React, { Component } from 'react';
-
-// class App extends Component {
-//   render() {
-//     return (
-//       <div className="container">
-//         <div className="row">
-//           <div className="col-sm-6 col-sm-offset-3">
-//             <form>
-//               <div className="form-group">
-//                 <label htmlFor="first-name">Nombre</label>
-//                 <input type="text" className="form-control" name="first-name" />
-//               </div>
-
-//               <div className="form-group">
-//                 <label htmlFor="last-name">Apellido</label>
-//                 <input type="text" className="form-control" name="last-name" />
-//               </div>
-
-//               <div className="action">
-//                 <button type="submit" className="btn btn-primary">Agregar Invitado</button>
-//               </div>
-//             </form>
-
-//             <table className="table bordered-table table-striped">
-//               <thead>
-//                 <tr>
-//                   <th>Nombre</th>
-//                   <th>Apellido</th>
-//                 </tr>
-//               </thead>
-//               <tbody>
-
-//               </tbody>
-//             </table>
-//           </div>
-//         </div>
-//       </div>
-//     )
-//   }
-// }
-
-// export default App
-
-
-
